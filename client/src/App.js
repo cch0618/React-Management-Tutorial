7d75d89052186e6e70005757df241a74355971b3
cch0618 <cch8529@naver.com>
2019-04-24 23:26:32 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -18,53 +18,45 @@ const styles = theme => ({
   table: {
     minWidth: 1080
   }
-})
-
-const customers = [
-  {
-  'id' : 1,
-  'image' : 'https://placeimg.com/64/64/3',
-  'name' : '최창훈',
-  'birthday' : '950626',
-  'gender' : '남자',
-  'job' : '대학생'
-},
-{
-  'id' : 2,
-  'image' : 'https://placeimg.com/64/64/2',
-  'name' : '이도훈',
-  'birthday' : '950626',
-  'gender' : '남자',
-  'job' : '대학생'
-},
-{
-  'id' : 3,
-  'image' : 'https://placeimg.com/64/64/3',
-  'name' : '중부인',
-  'birthday' : '950626',
-  'gender' : '남자',
-  'job' : '대학생'
-}
-]
+});
 
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+       .then(res => this.setState({customers: res}))
+       .catch(err => console.log(err));
+  }
+
+  callApi = async () => {//비동기적 수행
+  const response = await fetch('/api/customers');
+  const body = await response.json();
+  return body;
+}
+
   render() {
-    const { classes} = this.props;
+    const { classes } = this.props;
     return(
       <Paper className={classes.root}>
            <Table className={classes.table}>
              <TableHead>
                <TableRow>
                  <TableCell>번호</TableCell>
-                 <TableCell>이미지</TableCell>
-                 <TableCell>이름</TableCell>
-                 <TableCell>생년월일</TableCell>
-                 <TableCell>성별</TableCell>
-                 <TableCell>직업</TableCell>
+                 <TableCell>온도</TableCell>
+                 <TableCell>습도</TableCell>
+                 <TableCell>혼탁도</TableCell>
+                 <TableCell>토양수분</TableCell>
+                 <TableCell>펌프</TableCell>
                  </TableRow>
              </TableHead>
              <TableBody>
-       {customers.map(c=> { return(<Customer  key={c.id} id={c.id} image={c.image} name={c.name}  birthday={c.birthday} gender={c.gender} job={c.job}/> ); })  }
+       {this.state.customers ? this.state.customers.map(c=> { 
+         return(<Customer  key={c.id} id={c.id} image={c.image} name={c.name}  birthday={c.birthday} gender={c.gender} job={c.job}/> );
+         }) : ""}
        </TableBody>
        </Table>
        </Paper>
